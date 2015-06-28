e4996844c92601a403ab76deba46e7549a29f331
Simen Owesen-Lein <simen.owesenlein@gmail.com>
2015-06-28 21:40:31 +0200

    Started incorporating webrtc, does not work at the moment

--- fileTransfers.js
+++ fileTransfers.js
@@ -3,25 +3,66 @@
 
     app.factory('transfers', function(coral, fileTransfer) {
 
-        var listOfFileTransfers = {};
-
-        var fileTransfers = {};
-
-        fileTransfers.get = function(id) {
-            if (!listOfFileTransfers[id]) {
-                newTransfer(id);
-            }
-            return listOfFileTransfers[id];
-        };
+        var list = {};
 
         function newTransfer(id) {
             var transfer = fileTransfer.newTransfer();
-            listOfFileTransfers[id] = transfer;
-            transfer.setSender(
-                function(message) {
+            var peerConnection = createPeerConnection(id);
+
+            list[id] = {
+                peerConnection: peerConnection,
+                transfer: transfer,
+                sendOnCoral: function(message) {
                     coral.sendMessage(id, createFileMessage(message));
+                },
+                sendOnWebRTC: function(message) {
+
+                }
+            };
+
+            transfer.setSender(
+                list[id].sendOnWebRTC;
+            );
+
+            createPeerConnection(id);
+        }
+
+        function createPeerConnection(id, sendCallback) {
+            peerConnection = new RTCPeerConnection(options);
+
+            var dataChannel = peerConnection.createDataChannel("dataChannel", {
+                ordered: false
+            });
+
+            dataChannel.onopen = function() {
+                dataChannel.send(’hi’);
+            }
+
+            peerConnection.createOffer(function(offer) {
+                peerConnection.setLocalDescription(offer);
+
+                //send offer
+            });
+
+            return peerConnection;
+        }
+
+        function answer(offer) {
+            peerConnection.setRemoteDescription(
+                new RTCSessionDescription(offer),
+                function() {
+                    Success callback
+                },
+                function(err) {
+                    //Error callback
+                    console.log(err);
                 }
             );
+
+            peerConnection.createAnswer(function(answer) {
+                peerConnection.setLocalDescription(answer);
+                //send answer
+            });
         }
 
         function createFileMessage(message) {
@@ -31,7 +72,13 @@
             }
         }
 
-        return fileTransfers;
-
+        return {
+            get: function(id) {
+                if (!list[id]) {
+                    newTransfer(id);
+                }
+                return list[id].transfer;
+            }
+        }
     });
 })();
